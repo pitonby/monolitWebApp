63d8b716a57d1cdebc55fa2f1f85fbcc547b2eee
pitonby <atroshkin.dzmitry@gmail.com>
2017-05-31 14:21:03 +0300

    normal sql

---
+++ mon.sql
@@ -0,0 +1,26 @@
+﻿DROP DATABASE IF EXISTS `monolit`;
+
+CREATE DATABASE `monolit` DEFAULT CHARACTER SET utf8;
+
+USE `monolit`;
+
+CREATE TABLE IF NOT EXISTS apps(
+	id INT AUTO_INCREMENT NOT NULL,
+	firstName CHAR(255) NOT NULL,
+	secondName CHAR(255) NOT NULL,
+	department CHAR(255) NOT NULL,
+	id_number CHAR(7) NOT NULL,
+	pc_number CHAR(8) NOT NULL,
+	text TEXT NOT NULL,
+	startTime DATETIME NOT NULL,
+	endTime DATETIME,
+	flag ENUM ('Новое', 'Выполняется', 'Завершено'),
+	PRIMARY KEY(id)
+	)
+	ENGINE INNODB
+	DEFAULT CHARACTER SET utf8;
+	
+INSERT INTO apps
+(firstName, secondName, department, id_number, pc_number, text, startTime, flag)
+VALUES
+("Дмитрий", "Атрошкин", "ОИВК", "51-0081", "N1418092", "БЛА БЛА БЛА", NOW(), "Новое");
